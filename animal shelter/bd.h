5d271ee3fc15fbe1dac1b212a937b0ff9a486284
lizpar9990 <145855865+lizpar9990@users.noreply.github.com>
2024-11-12 00:23:45 +0300

    Create bd.h

---
+++ animal shelter/bd.h
@@ -0,0 +1,17 @@
+#include "AnimalClass.h"
+#include <vector>
+using namespace std;
+
+class BD {
+private:
+    vector<Animals> KotikiVec;
+    ulongint _NumberOfCats;
+public:
+void read();
+void save();
+void edit(uint _id);
+void add(Animals);
+void Delete(uint _id);
+void print();
+vector<Animals>search(unsigned int _id, bool _breed, bool _gender, unsigned short int _age, bool _status);
+    }
